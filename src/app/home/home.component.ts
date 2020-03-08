d98a9e941277bdaaf1809ae0bd4ad119de535436
tasos kakouris <tasoskakouris@gmail.com>
2020-03-08 23:55:53 +0200

    stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
+import * as mathjs from "mathjs";
 // constants
 @Component({
   selector: "app-home",
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
   SIZE = 50;
   ITERATIONS = 1000;
   loading: boolean;
+  energies = [];
   public graph = {
     data: [
       {
@@ -53,6 +54,7 @@ export class HomeComponent implements OnInit {
       }
       ///fml
       console.log(k);
+      this.energies.push(this.calculateTotalEnergy(this.voltageMatrix));
     }
     this.graph.data[0].z = this.voltageMatrix;
     this.graph.data[0].x = this.axis;
@@ -86,6 +88,34 @@ export class HomeComponent implements OnInit {
         }
       }
     }
+  }
+  private calculateTotalEnergy(a: any): number {
+    let sumOne = 0.0;
+    let sumTwo = 0.0;
+    for (let i = 0; i < this.SIZE; i++) {
+      for (let j = 0; j < this.SIZE; j++) {
+        let firstTerm = Math.pow(
+          this.getVoltage(i, j) - this.getVoltage(i - 1, j),
+          2
+        );
+        let secondTerm = Math.pow(
+          this.getVoltage(i, j) - this.getVoltage(i, j - 1),
+          2
+        );
+        let thirdTerm = this.chargeMatrix[i][j] * this.voltageMatrix[i][j];
+        sumOne += firstTerm + secondTerm;
+        if (i < this.SIZE - 2 || i < this.SIZE - 2) {
+          sumTwo += thirdTerm;
+        }
+      }
+    }
+    let final = (1.0 / 2.0) * sumOne - Math.pow(this.H, 2) * sumTwo;
+    return final;
+  }
+  private getVoltage(i: number, j: number) {
+    const isBound = this.isAtBoundaries(i, j);
+    if (isBound) return 0.0;
+    return this.voltageMatrix[i][j];
   }
   private fillChargeMatrixWithValues(): void {
     for (let i = 0; i < this.SIZE; i++) {
@@ -128,3 +158,16 @@ export class HomeComponent implements OnInit {
     return false;
   }
 }
+// const f1 = mathjs.subtract(mathjs.bignumber(1.0), mathjs.bignumber(this.OMEGA));
+// const f2 = mathjs.bignumber(this.voltageMatrix[i][j]);
+// const f3 = mathjs.divide(mathjs.bignumber(this.OMEGA), mathjs.bignumber(4.0));
+// const f4 = mathjs.bignumber(this.voltageMatrix[i + 1][j]);
+// const f5 = mathjs.bignumber(this.voltageMatrix[i - 1][j]);
+// const f6 = mathjs.bignumber(this.voltageMatrix[i][j + 1]);
+// const f7 = mathjs.bignumber(this.voltageMatrix[i][j - 1]);
+// const f8 = mathjs.bignumber(this.chargeMatrix[i][j]);
+// const sum1 = mathjs.add(f4, f5);
+// const sum2 = mathjs.add(f6, f7);
+// // const sum3 = mathjs.add(sum2, f8);
+// let sumFinal = mathjs.add(sum1, sum2);
+// const mult = mathjs.multiply(f1, f2);
