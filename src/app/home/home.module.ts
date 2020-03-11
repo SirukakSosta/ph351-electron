37a90d76d87bb9c75fd114c9dc09e49ba1a8ee26
tasos kakouris <tasoskakouris@gmail.com>
2020-03-11 02:18:14 +0200

    vector field

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,11 +7,39 @@ import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 import { SharedModule } from "../shared/shared.module";
 import { LoadingComponent } from "../components/loading/loading.component";
-import { ThreeDSurfaceComponent } from './three-d-surface/three-d-surface.component';
-import { LineComponent } from './line/line.component';
-
+import { ThreeDSurfaceComponent } from "./three-d-surface/three-d-surface.component";
+import { LineComponent } from "./line/line.component";
+import { VectorPlotComponent } from "./vector-plot/vector-plot.component";
+import {
+  HighchartsConfig,
+  HighchartsModule
+} from "@howtimeflies/ngx-highcharts";
+const config: HighchartsConfig = {
+  cdnBaseUrl: "https://code.highcharts.com",
+  scriptName: "highcharts.js",
+  delayToExecuteModulesCode: 200,
+  maxDelayToResizeContainer: 10000,
+  globalOptions: {
+    lang: {
+      drillUpText: "Drill-Up"
+    }
+  }
+};
 @NgModule({
-  declarations: [HomeComponent, LoadingComponent, ThreeDSurfaceComponent, LineComponent],
-  imports: [CommonModule, SharedModule, PlotlyModule, HomeRoutingModule]
+  declarations: [
+    HomeComponent,
+    LoadingComponent,
+    ThreeDSurfaceComponent,
+    LineComponent,
+    VectorPlotComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    PlotlyModule,
+    HomeRoutingModule,
+    HighchartsModule
+  ],
+  providers: [{ provide: HighchartsConfig, useValue: config }]
 })
 export class HomeModule {}
