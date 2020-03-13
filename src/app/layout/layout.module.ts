238a1576227a28a18f66add42d6a2b4ae162a266
tasos kakouris <tasos.kakouris@inlecomsystems.com>
2020-03-13 22:16:31 +0200

    styling menu

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 // import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { LoadingComponent } from "./loading/loading.component";
 import { NgZorroAntdModule } from "ng-zorro-antd";
+import { NzGridModule } from "ng-zorro-antd/grid";
+
 @NgModule({
-  imports: [CommonModule, NgZorroAntdModule],
+  imports: [CommonModule, NgZorroAntdModule, NzGridModule],
   declarations: [LoadingComponent],
-  exports: [LoadingComponent, NgZorroAntdModule]
+  exports: [LoadingComponent, NgZorroAntdModule, NzGridModule]
 })
 export class LayoutModule {}
