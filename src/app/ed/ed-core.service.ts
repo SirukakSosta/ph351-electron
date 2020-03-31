cbad9631d108e800d8f3e1e2218f1bb5b9ab7f82
tasos kakouris <tasos.kakouris@inlecomsystems.com>
2020-03-31 13:21:19 +0300

    create vector basis

--- src/app/ed/ed-core.service.ts
+++ src/app/ed/ed-core.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import * as MathJS from "mathjs";
 
+/** Constants */
+const N = 10; /** Grid size */
+const step = 1 / N; /** Step */
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class EdCoreService {
+  constructor() {
+    const initialVector = [];
+    const basisVectors = this.createVectorBase();
+    console.table(basisVectors);
+  }
 
-  constructor() { }
+  private createVectorBase(): Array<Array<number>> {
+    let basisVectors = new Array(N).fill(0).map(() => new Array(N).fill(0));
+    for (let row = 0; row < N; row++) {
+      for (let col = 0; col < N; col++) {
+        if (row === col) {
+          basisVectors[row][col] = 1;
+        }
+      }
+    }
+    return basisVectors;
+  }
 }
--- src/app/ed/ed-wrapper/ed-wrapper.component.ts
+++ src/app/ed/ed-wrapper/ed-wrapper.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { EdLabService } from '../ed-lab.service';
+import { Component, OnInit } from "@angular/core";
+import { EdLabService } from "../ed-lab.service";
+import { EdCoreService } from "../ed-core.service";
 
 @Component({
-  selector: 'app-ed-wrapper',
-  templateUrl: './ed-wrapper.component.html',
-  styleUrls: ['./ed-wrapper.component.scss']
+  selector: "app-ed-wrapper",
+  templateUrl: "./ed-wrapper.component.html",
+  styleUrls: ["./ed-wrapper.component.scss"]
 })
 export class EdWrapperComponent implements OnInit {
-
   // todo
   // play with https://github.com/sasekazu/visualize-jacobi-diagonalization/blob/master/js/main.js
 
-  constructor(private lab: EdLabService) { }
+  constructor(
+    // private lab: EdLabService,
+    private _edCoreService: EdCoreService
+  ) {}
 
   ngOnInit(): void {
-
     // this.lab.diagonalize()
-
   }
-
 }
