1f09adf3a1eb491155bff8639edd57ea21656dcd
tasos kakouris <tasos.kakouris@inlecomsystems.com>
2020-04-01 18:35:33 +0300

    remove console logs

--- src/app/ed/ed-core.service.ts
+++ src/app/ed/ed-core.service.ts
@@ -14,22 +14,12 @@ export class EdCoreService {
     let initialVector = [1, 1, 1, 0, 1, 2, 1, 0, 2, 1];
     initialVector = this.normalizeVector(initialVector);
     const basisVectors = this.createVectorBase(); /** IMPORTANT - vectors are in columns in this matrix */
-    console.log("BasiVector Created");
     const hamiltonianMatrix = this.hamiltonianMatrix(basisVectors);
-    console.log("hamiltonianMatrix Created");
-    console.table(hamiltonianMatrix);
     const ans = (<any>math).eigs(hamiltonianMatrix);
     const { values, vectors } = ans;
     const eigenValues = values;
     /** IMPORTANT - vectors are in rows in this matrix */
-    console.log("EIGEN VEC - BEFORE NORM");
-    console.table(vectors);
-    // const eigenVectors = this.normalizeEigenVectors(
-    //   vectors
-    // ); /** IMPORTANT - vectors are in rows in this matrix */
     const eigenVectors = vectors;
-    // console.table(values);
-    // console.table(vectors);
     return this.constractParts(
       initialVector,
       eigenValues,
@@ -59,19 +49,11 @@ export class EdCoreService {
 
     for (let i = 0; i < N; i++) {
       let k = 0;
-      // console.log(
-      //   "----------------------------------------------------------------------"
-      // );
-      // console.log("Hamiltonian N=", i);
-      /** Sunoriaki sinithiki */
       if (i === N - 1) {
         k = -1; /** gia na girisi sosta sti mideniki thesi */
       } else {
         k = i;
       }
-      // console.log("BRACKETS");
-      // console.log(`|${k + 1}><${i}|`);
-      // console.log(`|${i}><${k + 1}|`);
       const firstPartColumn = this.columnVector(basisVectors, k + 1);
       const firstPartRow = this.rowVector(basisVectors, i);
       const secondPartColumn = this.columnVector(basisVectors, i);
@@ -79,8 +61,7 @@ export class EdCoreService {
 
       const firstPart = this.calculateketBra(firstPartColumn, firstPartRow);
       const secondPart = this.calculateketBra(secondPartColumn, secondPartRow);
-      // console.table(firstPart);
-      // console.table(secondPart);
+
       /** Add the two matrices for N step */
       for (let row = 0; row < N; row++) {
         for (let col = 0; col < N; col++) {
@@ -89,10 +70,6 @@ export class EdCoreService {
             hamiltonian[row][col] + firstPart[row][col] + secondPart[row][col];
         }
       }
-      // console.table(tmp);
-      // console.log(
-      //   "----------------------------------------------------------------------"
-      // );
     }
 
     return hamiltonian;
@@ -108,15 +85,13 @@ export class EdCoreService {
     // lets find k = 4;
     const k = 8;
     let finalData = [];
-    for (let dt = 0.1; dt < 50; dt += 0.1) {
+    for (let dt = 0.1; dt < 50; dt += 0.01) {
       let realPart = 0;
       let imageinaryPart = 0;
       for (let i = 0; i < N; i++) {
         for (let m = 0; m < N; m++) {
           const Z_IM_PART = this.createZpart(m, i, eigenVectors, basisVectors);
           const Z_KM_PART = this.createZpart(m, k, eigenVectors, basisVectors);
-          // console.log("cos", Math.cos(eigenValues[m] * dt));
-          // console.log("sin", Math.cos(eigenValues[m] * dt));
           realPart =
             realPart +
             initialVector[i] *
@@ -131,14 +106,9 @@ export class EdCoreService {
               Math.sin(eigenValues[m] * dt);
         }
       }
-      //I DONT
-      // console.log("PARTS (R,I)", realPart, imageinaryPart);
       let magnitude = Math.pow(realPart, 2) + Math.pow(imageinaryPart, 2);
-      // magnitude = Math.sqrt(magnitude);
-      // (Math.pow(realPart, 2) + Math.pow(imageinaryPart, 2));
       finalData.push({ time: dt, mag: magnitude });
     }
-    // console.log("finaldata", finalData);
     return finalData;
   }
   private createZpart(m, i, eigenVectors, basisVectors): number {
@@ -193,11 +163,8 @@ export class EdCoreService {
       for (let col = 0; col < N; col++) {
         count = count + Math.pow(eigenVectors[row][col], 2);
       }
-      console.log("count is sqrt", Math.sqrt(count));
       for (let i = 0; i < N; i++) {
-        console.log("norm before", eigenVectors[row][i]);
         tmp[row][i] = eigenVectors[row][i] / Math.sqrt(count);
-        console.log("norm aftre", tmp[row][i]);
       }
     }
     return tmp;
